0921bb9b93240ec65696d42d2e2b4d345eaeabac
blobbyblobfish <aywang6@gmail.com>
2020-06-15 16:21:25 -0400

    added cocktail ingredients to /cocktails/ route

--- app/controllers/api/v1/cocktails_controller.rb
+++ app/controllers/api/v1/cocktails_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::CocktailsController < ApplicationController
     def index
         cocktails = Cocktail.all
 
-        render json: cocktails, except: [:created_at, :updated_at]
+        render json: cocktails, include: [:cocktail_ingredients], except: [:created_at, :updated_at]
     end
 
 end
