94af60cef74d3c4e3f3af1807dc649f4613367f2
blobbyblobfish <aywang6@gmail.com>
2020-06-15 17:25:00 -0400

    added ingredients show route

--- app/controllers/api/v1/ingredients_controller.rb
+++ app/controllers/api/v1/ingredients_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::IngredientsController < ApplicationController
+
+    def show
+        ingredient = Ingredient.find(params[:id])
+
+        render json: ingredient
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :cocktails
+      get '/cocktails', to: 'cocktails#index', as: 'cocktails'
+      get '/ingredients/:id', to: 'ingredients#show', as: 'ingredient'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
